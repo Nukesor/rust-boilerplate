0af6c5f8b2def210bf31bb67f29f24309bb2a8dc
Arne Beer <contact@arne.beer>
2022-11-01 21:26:50 +0100

    chore: Bump deps

--- src/cli.rs
+++ src/cli.rs
@@ -1,9 +1,9 @@
-use clap::Parser;
+use clap::{ArgAction, Parser};
 
 #[derive(Parser, Debug)]
 #[clap(name = "rust-boilerplate", about = "Some about", author, version)]
 pub struct CliArguments {
     /// Verbose mode (-v, -vv, -vvv)
-    #[clap(short, long, parse(from_occurrences))]
+    #[clap(short, long, action = ArgAction::Count)]
     pub verbose: u8,
 }
